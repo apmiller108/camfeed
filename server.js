34adfcaef9bbb6c35e967684a83d5ae76b74b1fe
Alex Miller <apmiller108@yahoo.com>
2016-05-30 20:25:01 -0400

    Connect to Camfeed API over sockets and join room

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 'use strict';
 let feed = require('./feed');
+let socket = require('socket.io-client')('http://192.168.1.192:3000');
 
 function initialize() {
-  console.log(`raspivid started with PID: ${feed.raspivid.pid}`);
+  console.log('Camfeed PI utility started');
+
 }
+
+socket.on('connect', () => {
+  socket.emit('connection made', 'Camfeed PI is connected');
+  console.log('Connected to API');
+
+  socket.emit('room', '1234');
+  socket.on('room', (data) =>{
+    console.log(data);
+  });
+});
 
 initialize();
 
-feed.initialize();
+// feed.initialize();
